21aa0cf0251de6ec6b5a43554ed13912fdc4ef76
Allen <18328073611@163.com>
2018-11-23 00:21:50 +0800

    - [add] 新增数据结构——链表

---
+++ src/linked-list.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  append(element) {
+    const node = new Node(element);
+    let current;
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.length++;
+  }
+
+  insert(position, element) {}
+
+  removeAt(position) {
+    if (position > -1 && position < this.length) {
+      let [current, index, previous] = [this.head, 0];
+      if (position === 0) {
+        this.head = current.next;
+      } else {
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+      this.length--;
+      return current.element;
+    } else {
+      return null;
+    }
+  }
+
+  remove(element) {}
+
+  indexOf(element) {}
+
+  isEmpty() {}
+
+  size() {}
+
+  getHead() {}
+
+  toString() {}
+}
