44baacf07e5017fc188465dd484747dcf80bbd1f
Allen <18328073611@163.com>
2019-01-06 14:23:05 +0800

    - [add] 完善二叉树

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -142,4 +142,76 @@ class BinarySearchTree {
     }
     return null;
   }
+
+  /**
+   * 搜索在树中是否存在某个值
+   * @param {*} key 要搜索的值
+   * @returns {boolean} 该值是否存在于树中
+   */
+  search(key) {
+    return BinarySearchTree.searchNode(this.root, key);
+  }
+
+  /**
+   * 搜索树中节点的值
+   * @param {Node} node 树的节点
+   * @param {*} key 值
+   * @returns {boolean} 该节点的值是否是要检索的值
+   */
+  static searchNode(node, key) {
+    if (node === null) {
+      return false;
+    }
+    if (key < node.key) {
+      return BinarySearchTree.searchNode(node.left, key);
+    } else if (key > node.key) {
+      return BinarySearchTree.searchNode(node.right, key);
+    } else {
+      return true;
+    }
+  }
+
+  /**
+   * 根据值移除树的节点
+   * @param key 节点的值
+   */
+  remove(key) {
+    this.root = BinarySearchTree.removeNode(this.root, key);
+  }
+
+  static removeNode(node, key) {
+    if (node === null) {
+      return null;
+    }
+    if (key < node.key) {
+      node.left = BinarySearchTree.removeNode(node.left, key);
+      return node;
+    } else if (key > node.key) {
+      node.right = BinarySearchTree.removeNode(node.right, key);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+      const aux = BinarySearchTree.findMinNode(node.right);
+      node.key = aux.key;
+      node.right = this.removeNode(node.right, aux.key);
+      return node;
+    }
+  }
+
+  static findMinNode(node) {
+    while (node && node.left !== null) {
+      node = node.left;
+    }
+    return node;
+  }
 }
